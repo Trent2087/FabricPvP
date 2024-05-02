b13c8be03715ae83b9ad0ddee9750eb7498b3f78
Michael Wood <jackofthewisps@gmail.com>
2024-05-02 00:40:06 -0700

    Update to 1.19.2+
    Works for 1.19.2 and 1.20.1. Probably more.

--- src/main/java/mod/trent2087/fabricpvp/FabricPvP.java
+++ src/main/java/mod/trent2087/fabricpvp/FabricPvP.java
@@ -3,12 +3,11 @@ package mod.trent2087.fabricpvp;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.TranslatableText;
-
+import net.minecraft.text.Text;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -18,7 +17,7 @@ import static net.minecraft.server.command.CommandManager.literal;
 public class FabricPvP implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, listener) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(literal("pvp")
                     .executes(this::togglePvP)
                     .then(literal("on")
@@ -33,20 +32,19 @@ public class FabricPvP implements ModInitializer {
     }
 
     private int setPvP(CommandContext<ServerCommandSource> ctx, boolean b) throws CommandSyntaxException {
-        return setPvP(ctx, b, Collections.singletonList(ctx.getSource().getPlayer()));
+        return setPvP(ctx, b, Collections.singletonList(ctx.getSource().getPlayerOrThrow()));
     }
     private int setPvP(CommandContext<ServerCommandSource> ctx, boolean b, Collection<ServerPlayerEntity> players) {
         players.forEach(v -> {
             PlayerComponentInitializer.PVP_DATA.get(v).set(b);
         });
-        ctx.getSource().sendFeedback(new TranslatableText(players.size() == 1 ? "You have %s PvP" : "You have %s PvP for all selected players", b ? "enabled" : "disabled"), players.size() > 1);
         return 1;
     }
 
     private int togglePvP(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        PlayerComponentInitializer.IPvPComponent pvpComponent = PlayerComponentInitializer.PVP_DATA.get(context.getSource().getPlayer());
+        PlayerComponentInitializer.IPvPComponent pvpComponent = PlayerComponentInitializer.PVP_DATA.get(context.getSource().getPlayerOrThrow());
         pvpComponent.set(!pvpComponent.isOn());
-        context.getSource().sendFeedback(new TranslatableText("You have %s PvP", pvpComponent.isOn() ? "enabled" : "disabled"), false);
+        context.getSource().sendMessage(Text.literal("").append(Text.literal("PVP status is - ")).append(pvpComponent.isOn() ? "enabled" : "disabled"));
         return 1;
     }
 }
